de91a4f7a634fffc832e2df5592b700b04f77326
dvaJi <francisco_p@outlook.com>
2018-03-06 20:00:21 -0300

    fix(Reader): Se añade salto de línea para cada imágen

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const READER_PATH = "/lector/api/"; // /lector/api/
-export const BLOG_PATH = "http://ravens-scans.com/rs-blog/wp-json/wp/v2";
-export const APP_PATH = "/"; // /multi2
+export const READER_PATH = "/api/";
+export const BLOG_PATH = "/wp-json/wp/v2";
+export const APP_PATH = "/";
 export const APP_TITLE = "Ravens Scans";
 export const APP_URL = "https://ravens-scans.com";
 export const DISQUS_SHORTNAME = "ravensfansub";
