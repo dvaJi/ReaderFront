6e8a7153c3b0939f5215a22a8710d6ff09447208
dvaJi <francisco_p@outlook.com>
2018-03-06 19:35:54 -0300

    fix(Blog): Corrección de estilos del módulo Blog

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const READER_PATH = "/lector/api/"; // /lector/api/
-export const BLOG_PATH = "/rs-blog/wp-json/wp/v2";
+export const BLOG_PATH = "http://ravens-scans.com/rs-blog/wp-json/wp/v2";
 export const APP_PATH = "/"; // /multi2
 export const APP_TITLE = "Ravens Scans";
 export const APP_URL = "https://ravens-scans.com";
