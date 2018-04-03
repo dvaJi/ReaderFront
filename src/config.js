10f2d6330761801bd9c4129364cd7e3b874d4cd9
dvaJi <francisco_p@outlook.com>
2018-04-02 22:04:38 -0300

    Configuración por defecto.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const READER_PATH = "/lector/api/";
 export const BLOG_PATH = "/wp-json/wp/v2";
 export const APP_PATH = "/";
-export const APP_TITLE = "Ravens Scans";
-export const APP_URL = "https://ravens-scans.com";
-export const DISQUS_SHORTNAME = "ravensfansub";
-export const GA_ID = "UA-112471783-1";
+export const APP_TITLE = "Reader Front";
+export const APP_URL = "https://example.com";
+export const DISQUS_SHORTNAME = "your_disqus_shortname_id";
+export const GA_ID = "UA-XXXXXXXXX-X";
