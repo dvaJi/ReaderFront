a9165a0c7433f0cb0775cab1283903c647cd7781
dvaJi <francisco_p@outlook.com>
2018-04-06 20:47:49 -0300

    chore: Dependencias actualizadas (React 16.3, react-scripts, font-awesome, etc.)

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const READER_PATH = "/lector/api/";
+export const READER_PATH = "/api/";
 export const BLOG_PATH = "/wp-json/wp/v2";
 export const APP_PATH = "/";
 export const APP_TITLE = "Reader Front";
