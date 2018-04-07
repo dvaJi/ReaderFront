62870ca37857ada16f991b58934292914deb7ecd
dvaJi <francisco_p@outlook.com>
2018-04-06 22:32:58 -0300

    feat: Links de Discord y Patreon ahora son configurables

--- src/config.js
+++ src/config.js
@@ -5,3 +5,5 @@ export const APP_TITLE = "Reader Front";
 export const APP_URL = "https://example.com";
 export const DISQUS_SHORTNAME = "your_disqus_shortname_id";
 export const GA_ID = "UA-XXXXXXXXX-X";
+export const DISCORD_URL = "https://discord.gg/YOUR_CODE";
+export const PATREON_URL = "https://www.patreon.com/YOUR_URL";
