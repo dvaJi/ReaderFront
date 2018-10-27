017568d259031656a57607232966338ba2d63e74
Francisco Pizarro <francisco_p@outlook.com>
2018-10-27 15:09:55 -0300

    feat: use env variables instead plain js config (#155)

--- src/config.js
+++ src/config.js
@@ -1,10 +1,14 @@
-export const READER_PATH = "http://localhost:8000/";
-export const BLOG_PATH = "/wp-json/wp/v2";
-export const APP_PATH = "/";
-export const APP_TITLE = "Reader Front";
-export const APP_URL = "https://example.com";
-export const DISQUS_SHORTNAME = "your_disqus_shortname_id";
-export const GA_ID = "UA-XXXXXXXXX-X";
-export const DISCORD_URL = "https://discord.gg/YOUR_CODE";
-export const DISCORD_ID = "YOUR_DISCORD_ID";
-export const PATREON_URL = "https://www.patreon.com/YOUR_URL";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const NODE_ENV = process.env.NODE_ENV;
+export const APP_URL = process.env.APP_URL;
+export const READER_PATH = process.env.READER_PATH;
+export const APP_PATH = process.env.APP_PATH;
+export const APP_TITLE = process.env.APP_TITLE;
+export const DISQUS_SHORTNAME = process.env.DISQUS_SHORTNAME;
+export const GA_ID = process.env.GA_ID;
+export const DISCORD_URL = process.env.DISCORD_URL;
+export const DISCORD_ID = process.env.DISCORD_ID;
+export const PATREON_URL = process.env.PATREON_URL;
