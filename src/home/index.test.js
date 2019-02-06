f7c317aef3552b94f37f85a38fd478e870fb9a4b
dvaJi <francisco_p@outlook.com>
2019-02-05 23:00:46 -0300

    feat(home): refactor Home

---
+++ src/home/index.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import { mountWithIntl } from 'enzyme-react-intl';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { Provider } from 'react-redux';
+
+import HomeContainer from './';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+it('should render without throwing an error', () => {
+  const store = mockStore({
+    layout: {
+      language: 'es'
+    }
+  });
+  const wrapper = mountWithIntl(
+    <MockedProvider mocks={[]} addTypename={false}>
+      <Provider store={store}>
+        <HomeContainer />
+      </Provider>
+    </MockedProvider>
+  );
+
+  expect(wrapper).toBeTruthy();
+  wrapper.unmount();
+});
