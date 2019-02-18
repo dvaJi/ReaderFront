4b69894e58ce5cf90862ee321a96d5e709af3e58
dvaJi <francisco_p@outlook.com>
2019-02-18 00:21:59 -0300

    chore: remove unnecessary redux files

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -3,8 +3,6 @@ import { connectRouter } from 'connected-react-router';
 import { intlReducer } from 'react-intl-redux';
 
 import releases from './releases/reducers';
-import works from './works/reducers';
-import work from './work/reducers';
 import reader from './reader/reducers';
 import layout from './layout/reducers';
 import user from './user/reducers/applyUser';
@@ -12,8 +10,6 @@ import user from './user/reducers/applyUser';
 export default history =>
   combineReducers({
     releases,
-    works,
-    work,
     reader,
     layout,
     user,
