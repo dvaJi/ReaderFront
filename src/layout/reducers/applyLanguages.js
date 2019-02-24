0499e191dc10f81e2b77f8cdf6efd3747fba2011
Francisco Pizarro <francisco_p@outlook.com>
2019-02-24 14:51:05 -0300

    fix: refactor layout reducer and action to use intl (#237)

--- src/layout/reducers/applyLanguages.js
+++ src/layout/reducers/applyLanguages.js
@@ -1,25 +1,4 @@
-function getDefaultLanguage() {
-  const navigatorLang = navigator.language || navigator.userLanguage || 'en';
-  const localLang = window.localStorage.getItem('rf_language');
-
-  let language = '';
-  if (localLang) {
-    language = localLang;
-  } else {
-    const validLanguages = ['es', 'en'];
-    const navigatorValue = navigatorLang.split('-')[0];
-    const isValidLanguage = validLanguages.indexOf(navigatorValue) > -1;
-    language = isValidLanguage ? navigatorValue : 'en';
-  }
-
-  if (localStorage.getItem('rf_language') !== language) {
-    localStorage.setItem('rf_language', language);
-  }
-
-  return language;
-}
-// TODO: revisar porque no funciona correctamente al inicio de la app, aparece seleccionado ES pero el idioma que aparece sigue siendo inglés
-// ES PORQUE ES EL REDUCER DEL LAYOUR Y NO DE react-intl
+import { getDefaultLanguage } from '../../utils/common';
 
 let initialState = getDefaultLanguage();
 
