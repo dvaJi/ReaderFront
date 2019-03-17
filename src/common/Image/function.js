62fa66c26c507968ec0c95f476fc5b6b68e1d66a
Francisco Pizarro <francisco_p@outlook.com>
2019-03-17 11:37:00 -0300

    fix: chapters are not being filtered by language in Work view (#250)

--- src/common/Image/function.js
+++ src/common/Image/function.js
@@ -2,12 +2,12 @@ import { IS_PROD, READER_PATH } from '../../config';
 import { getImage } from './load';
 
 export default function Image(src, height, width, index = 1) {
-  const href = IS_PROD ? READER_PATH + src : src;
+  const href = READER_PATH + src;
   const item = {
     href,
     height,
     width
   };
 
-  return !IS_PROD ? getImage(item, index) : href;
+  return IS_PROD ? getImage(item, index) : href;
 }
