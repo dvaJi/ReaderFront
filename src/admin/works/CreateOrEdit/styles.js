fb4837a872165e151dfb0ec22be2de2c7ec16f22
Francisco Pizarro <francisco_p@outlook.com>
2019-08-15 15:00:28 -0400

    feat(admincp): implement genres in works (#382)

---
+++ src/admin/works/CreateOrEdit/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const GenresWrapper = styled.div`
+  display: block;
+
+  & > div {
+    margin: 5px 10px;
+    display: inline-block;
+    width: 30%;
+  }
+`;
