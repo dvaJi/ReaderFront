17fcd7ed399ad42d7fafcf39047ae0ca33bf0e27
dvaJi <francisco_p@outlook.com>
2019-09-01 13:38:50 -0400

    feat(reader): implement a better lazyloading image

---
+++ src/common/Image/lazy-component.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+import { IS_PROD, READER_PATH } from '../../config';
+import { getImage } from './load';
+
+export default function Image({
+  src,
+  alt,
+  height,
+  width,
+  crop = false,
+  index = 1,
+  ...props
+}) {
+  const href = READER_PATH + (src || 'images/default-cover.png');
+  const item = {
+    href,
+    height,
+    width,
+    crop
+  };
+  const finalUrl = IS_PROD ? getImage(item, index) : href;
+
+  const lowResItem = {
+    ...item,
+    height: height > 0 ? Math.round(height / 5) : height,
+    width: width > 0 ? Math.round(width / 5) : width,
+    quality: 10
+  };
+  const lowResUrl = IS_PROD ? getImage(lowResItem, index) : href;
+  return (
+    <LazyLoadImage
+      src={finalUrl}
+      alt={alt}
+      height={height}
+      width={width}
+      key={finalUrl}
+      placeholderSrc={lowResUrl}
+      effect="blur"
+      scrollPosition={'vertical'}
+      {...props}
+    />
+  );
+}
