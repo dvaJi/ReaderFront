bed5f783ea24286cb8703765030fb0bb0f00e463
dvaJi <francisco_p@outlook.com>
2021-06-04 22:36:21 -0400

    feat: improve releases page

--- packages/web/src/components/Releases/styles.js
+++ packages/web/src/components/Releases/styles.js
@@ -2,12 +2,7 @@ import styled from 'styled-components';
 import theme from 'styled-theming';
 
 import { theme as rfTheme } from '@readerfront/ui';
-const { background, cardBackgroundColor, toRgb } = rfTheme;
-
-const hoverRow = theme('mode', {
-  light: toRgb(background.light.darkest),
-  dark: toRgb(background.dark.lighter)
-});
+const { background, toRgb } = rfTheme;
 
 const borderColor = theme('mode', {
   light: background.light.dark,
@@ -19,75 +14,102 @@ export const contentColor = theme('mode', {
   dark: background.dark.lighter
 });
 
+const shadowColor = theme('mode', {
+  light: toRgb(background.light.darkest),
+  dark: toRgb(background.dark.lighter)
+});
+
+export const Media = styled.div`
+  position: relative;
+  display: block;
+  padding: 0;
+  flex-shrink: 0;
+  border-radius: inherit;
+  transition: box-shadow 0.15s linear;
+
+  &:hover {
+    box-shadow: 0 0 20px rgba(${shadowColor}, 0.5);
+  }
+
+  &:after {
+    content: '';
+    display: block;
+    padding-top: 120%;
+  }
+`;
+
+export const MediaContent = styled.a`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border: 0;
+  border-radius: inherit;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-color: hsla(0, 0%, 47.1%, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const MediaOverlay = styled.div`
+  position: absolute;
+  top: ${({ top }) => (top ? '0' : 'auto')};
+  left: 0;
+  right: 0;
+  bottom: ${({ bottom }) => (bottom ? '0' : 'auto')};
+  padding: 1rem;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  color: #fff;
+`;
+
+export const Badge = styled.div`
+  padding: 0.3em 0.45em;
+  margin-right: 5px;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.51);
+
+  & > .status {
+    line-height: 2em;
+  }
+`;
+
+export const ListContent = styled.div`
+  padding-top: 0.5rem;
+  padding-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  justify-content: center;
+`;
+
+export const ListBody = styled.div`
+  flex: 1 1 auto;
+`;
+
+export const ListTitle = styled.a`
+  line-height: 1.4285714286;
+  font-weight: 500;
+  display: block;
+  color: inherit;
+  cursor: pointer;
+`;
+
+export const LastUpdate = styled.div`
+  font-size: 0.8rem;
+`;
+
 export const Paginator = styled.div`
   height: 38px;
   margin-bottom: 10px;
 `;
 
-export const ReleaseRow = styled.a`
-  display: inherit;
-  color: inherit;
-  text-decoration: none !important;
-  cursor: pointer;
-
-  &:hover {
-    background: rgba(${hoverRow}, 0.06);
-  }
-`;
-
-export const ReleaseCard = styled.div`
-  background-color: ${cardBackgroundColor};
-`;
-
-export const ReleaseContent = styled.p`
-  border-bottom: 1px solid ${borderColor};
-
-  strong {
-    color: ${contentColor};
-  }
-`;
-
-export const ReleaseChapterBlock = styled.span`
-  height: 32px;
-  width: 32px;
-  text-align: center;
-  color: ${contentColor};
-  position: relative;
-`;
-
 export const ReleaseTitle = styled.h6`
   border-bottom: 1px solid ${borderColor};
-`;
-
-export const ReleaseSkeletonLoading = styled.div`
-  background-color: ${cardBackgroundColor};
-
-  .media > span {
-    height: 32px;
-    width: 32px;
-  }
-
-  .media > p {
-    border-bottom: 1px solid ${borderColor};
-  }
-
-  h6,
-  strong,
-  .media > span {
-    background-color: rgba(${hoverRow}, 0.2);
-  }
-
-  h6 {
-    height: 21px;
-    width: 350px;
-  }
-
-  strong {
-    height: 15px;
-  }
-`;
-
-export const FlagWrapper = styled.div`
-  background-color: ${cardBackgroundColor};
-  position: absolute;
 `;
